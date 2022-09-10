a271cb0569d6c901053cbc6ebcd9d33a1032ada2
silver <silvi97lila@gmail.com>
2022-09-10 17:52:11 +0200

    feat: add sample test

--- src/components/tabs/index.tsx
+++ src/components/tabs/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Tab } from '@headlessui/react';
 import React from 'react';
 
---
+++ test/main.test.ts
@@ -0,0 +1,5 @@
+describe('Init test', () => {
+  it('should pass', () => {
+    expect(true).toBeTruthy();
+  });
+});
