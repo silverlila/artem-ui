a1e0603596dc6c0f712323a427e372ae4a445e1d
silver <silvi97lila@gmail.com>
2022-09-12 15:55:21 +0200

    refactor: alert component

--- src/components/alerts/index.tsx
+++ src/components/alerts/index.tsx
@@ -16,19 +16,23 @@ const alertTypeIcon: Record<'success' | 'error' | 'warning', JSX.Element> = {
   warning: <ExclamationTriangleIcon className="h-5 w-5 text-yellow-400" />,
 };
 const alertTypeColor: Record<'success' | 'error' | 'warning', string> = {
-  success: 'bg-green-50',
-  error: 'bg-red-50',
-  warning: 'bg-yellow-50',
+  success: 'green',
+  error: 'red',
+  warning: 'yellow',
 };
 export function Alert({ type, title, message }: AlertProps) {
   const icon: any = alertTypeIcon[type];
   return (
-    <div className={`rounded-md ${alertTypeColor[type]} p-4`}>
+    <div className={`rounded-md border bg-${alertTypeColor[type]}-50 p-4`}>
       <div className="flex">
         <div className="flex-shrink-0">{icon}</div>
         <div className="ml-3">
-          <h3 className="text-sm font-medium text-yellow-800">{title}</h3>
-          <div className="mt-2 text-sm text-yellow-700">
+          <h3
+            className={`text-sm font-medium text-${alertTypeColor[type]}-800`}
+          >
+            {title}
+          </h3>
+          <div className={`mt-2 text-sm text-${alertTypeColor[type]}-700`}>
             <p>{message}</p>
           </div>
         </div>
